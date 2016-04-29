d3971df0d53eed8d758bd3aa0d85c37d28307f6d
vasiliy.skidanenko <vasiliy.skidanenko@antarasoft.com>
2016-04-29 16:19:46 +0300

    Initial

---
+++ FindInCn.Shared/Models/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindInCn.Shared.Models
+{
+    public class Shop
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ShopId { get; set; }
+        public string Name { get; set; }
+        public string SearchUrl { get; set; }
+        public string MainPage { get; set; }
+    }
+}
--- FindInCn/Global.asax.cs
+++ FindInCn/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FindInCn.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,29 @@ namespace FindInCn
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        void Init()
+        {
+            var c = new CnContext();
+            c.Shops.Add(
+                new Shop()
+                {
+                    Name = "AliExpress",
+                    SearchUrl = "http://aliexpress.com/wholesale?SearchText={0}",
+                    MainPage = "http://ru.aliexpress.com/ru_home.htm"
+                });
+
+            c.Shops.Add(
+                new Shop()
+                {
+                    Name = "GearBest",
+                    SearchUrl = "http://www.gearbest.com/{0}-_gear/",
+                    MainPage = "http://www.gearbest.com/"
+                });
+        }
+
         protected void Application_Start()
         {
+            Init();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
